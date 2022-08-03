a407f5ab6f3d50adfc9431539320ba95211fc608
Imani Mwamlima <imwamlima@gmail.com>
2022-08-03 01:46:34 -0400

    add correct port for server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -53,6 +54,6 @@ app.post("/", function(req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server is running on port 3000");
 });
